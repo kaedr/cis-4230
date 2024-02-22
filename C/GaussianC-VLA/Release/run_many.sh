f76f6da8d390845660173908658ab613baa3e6e4
Henry Corse <kaedraar@gmail.com>
2024-02-22 10:41:17 -0500

    updating runmany.sh

--- C/GaussianC-VLA/Release/run_many.sh
+++ C/GaussianC-VLA/Release/run_many.sh
@@ -1,29 +1,38 @@
+min_number() {
+    printf "%sms\n" "$@" | sort -g | head -n1
+}
+
 echo "Running 1000x1000"
-./GaussianC-VLA.exe ~/1000x1000.dat
-./GaussianC-VLA.exe ~/1000x1000.dat
-./GaussianC-VLA.exe ~/1000x1000.dat
+ONE=$(./GaussianC-VLA.exe ~/1000x1000.dat | cut -d ' ' -f4)
+TWO=$(./GaussianC-VLA.exe ~/1000x1000.dat | cut -d ' ' -f4)
+THREE=$(./GaussianC-VLA.exe ~/1000x1000.dat | cut -d ' ' -f4)
+min_number $ONE $TWO $THREE
 
 echo "Running 1250x1250"
-./GaussianC-VLA.exe ~/1250x1250.dat
-./GaussianC-VLA.exe ~/1250x1250.dat
-./GaussianC-VLA.exe ~/1250x1250.dat
+ONE=$(./GaussianC-VLA.exe ~/1250x1250.dat | cut -d ' ' -f4)
+TWO=$(./GaussianC-VLA.exe ~/1250x1250.dat | cut -d ' ' -f4)
+THREE=$(./GaussianC-VLA.exe ~/1250x1250.dat | cut -d ' ' -f4)
+min_number $ONE $TWO $THREE
 
 echo "Running 1500x1500"
-./GaussianC-VLA.exe ~/1500x1500.dat
-./GaussianC-VLA.exe ~/1500x1500.dat
-./GaussianC-VLA.exe ~/1500x1500.dat
+ONE=$(./GaussianC-VLA.exe ~/1500x1500.dat | cut -d ' ' -f4)
+TWO=$(./GaussianC-VLA.exe ~/1500x1500.dat | cut -d ' ' -f4)
+THREE=$(./GaussianC-VLA.exe ~/1500x1500.dat | cut -d ' ' -f4)
+min_number $ONE $TWO $THREE
 
 echo "Running 1750x1750"
-./GaussianC-VLA.exe ~/1750x1750.dat
-./GaussianC-VLA.exe ~/1750x1750.dat
-./GaussianC-VLA.exe ~/1750x1750.dat
+ONE=$(./GaussianC-VLA.exe ~/1750x1750.dat | cut -d ' ' -f4)
+TWO=$(./GaussianC-VLA.exe ~/1750x1750.dat | cut -d ' ' -f4)
+THREE=$(./GaussianC-VLA.exe ~/1750x1750.dat | cut -d ' ' -f4)
+min_number $ONE $TWO $THREE
 
 echo "Running 2000x2000"
-./GaussianC-VLA.exe ~/2000x2000.dat
-./GaussianC-VLA.exe ~/2000x2000.dat
-./GaussianC-VLA.exe ~/2000x2000.dat
+ONE=$(./GaussianC-VLA.exe ~/2000x2000.dat | cut -d ' ' -f4)
+TWO=$(./GaussianC-VLA.exe ~/2000x2000.dat | cut -d ' ' -f4)
+THREE=$(./GaussianC-VLA.exe ~/2000x2000.dat | cut -d ' ' -f4)
+min_number $ONE $TWO $THREE
 
-echo "Running 5000x5000"
-./GaussianC-VLA.exe ~/5000x5000.dat
-./GaussianC-VLA.exe ~/5000x5000.dat
-./GaussianC-VLA.exe ~/5000x5000.dat
+# echo "Running 5000x5000"
+# ./GaussianC-VLA.exe ~/5000x5000.dat
+# ./GaussianC-VLA.exe ~/5000x5000.dat
+# ./GaussianC-VLA.exe ~/5000x5000.dat
